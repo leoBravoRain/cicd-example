aa180c1c2e382e3fd4185d98676643a9dc04cbc6
leo <leo.bravo.rain@gmail.com>
2021-12-07 14:17:11 -0300

    rchange word to text linked with test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Webpage created by Leo Bravo</p>
+        <p>Content created by Leo Bravo</p>
         <p>
           hello <code>src/App.js</code> and save to reload.
         </p>
