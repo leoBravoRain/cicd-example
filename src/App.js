26628a5500e4b8fb8b0cb7f76dd4fbc48b2ec9b5
leo <leo.bravo.rain@gmail.com>
2021-12-07 14:20:46 -0300

    fix bug

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Content created by Leo Bravo</p>
+        <p>Webpage created by Leo Bravo</p>
         <p>
           hello <code>src/App.js</code> and save to reload.
         </p>
